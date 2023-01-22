03d68f5d49d741f5107909c42a0d2c943f32de26
Georgii Shishko <vindestr@gmail.com>
2023-01-22 19:17:08 +0300

    запись в json и тест нескольких страниц сразу

--- main.ts
+++ main.ts
@@ -1,16 +1,40 @@
 const Crawler = require('crawler')
+const fs = require("fs")
 
-const obj = {}
+// const obj = {}
+
+function addSymptomsToFile(diseaseName, symptoms)
+{
+  const rawData = fs.readFileSync('data.json')
+  const newData = JSON.stringify({ ...rawData, ...{ [diseaseName]: symptoms } })
+  fs.writeFileSync('data.json', newData)
+}
+
+function getQueue() {
+  const diseaseLinks = [];
+  for (let i = 0; i <= 10000; i++) {
+    diseaseLinks.push(`https://omim.org/clinicalSynopsis/${i}`);
+  }
+  return diseaseLinks;
+}
+
 const crawlerInstance = new Crawler({
   maxConnections: 10,
-  callback: (error, res, done) =>
+  callback      : (error, res, done) =>
   {
+    const obj = {}
+    
     if (error)
     {
-      console.log(error)
     } else
     {
       const $ = res.$
+      const diseaseName = $('#mimContent > div.container.hidden-print > div:nth-child(2) >' +
+        ' div.col-lg-8.col-lg-pull-2.col-md-8.col-md-pull-2.col-sm-8.col-sm-pull-2.col-xs-12 > div:nth-child(1) >' +
+        ' div:nth-child(4) > h3 > span').text().trim()
+      
+      if (!diseaseName) done();
+      console.log(diseaseName)
       const organSystemContainer =
         $('#mimClinicalSynopsis > div ')
       organSystemContainer.each(function (index, element)
@@ -24,10 +48,11 @@ const crawlerInstance = new Crawler({
           obj[title] = symptoms
         }
       })
+      addSymptomsToFile(diseaseName, obj)
+      console.log(obj)
+      done()
     }
-    console.log(obj)
-    done()
   }
 })
 
-crawlerInstance.queue('https://omim.org/clinicalSynopsis/162200')
+crawlerInstance.queue(getQueue())
