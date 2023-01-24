89e1a1bbad94d24d7f97b44da28e993079787c0d
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2023-01-24 16:04:16 +0300

    Удалил неиспользуемый код

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,19 @@
-import * as fs from "fs"
 import {downloadDom} from "@/crawler/downloadDom.js";
 import {parseOmimClinicalSynopsis} from "@/parsers/parseOmimClinicalSynopsis.js";
 import {parsersOMIMDisorders} from "@/parsers/parsersOMIMDisorders.js";
 import {write} from "@/io/saveFile.js";
 
-function addSymptomsToFile(diseaseName, symptoms)
-{
-  const rawData = fs.readFileSync('data.json')
-  const newData = JSON.stringify({ ...rawData, ...{ [diseaseName]: symptoms } })
-  fs.writeFileSync('data.json', newData)
-}
-
-const ids = ['162200']
-
-export async function parseOmim(url: string) {
+export async function parseOMIM(url: string) {
   const dom = await downloadDom(url);
   return parseOmimClinicalSynopsis(dom.window.document.body)
 }
 export async function parse_list_of_OMIM_disorder_codes() {
-  const dom = await downloadDom("https://en.wikipedia.org/wiki/List_of_OMIM_disorder_codes");
+  const url = "https://en.wikipedia.org/wiki/List_of_OMIM_disorder_codes";
+  const dom = await downloadDom(url);
   return parsersOMIMDisorders(dom.window.document.body);
 }
 
-parseOmim('https://omim.org/clinicalSynopsis/162200').then(result => {
+parseOMIM('https://omim.org/clinicalSynopsis/162200').then(result => {
   saveObject('162200.json', result)
 })
 
